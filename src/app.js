85af757c03f1788c253da6172978e1fc6b31b63e
JackW <jack.wen@ec.gc.ca>
2015-08-25 11:02:10 -0400

    chore(doc): add module description
    add sample module description to esri.map module

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 (function(angular) {
     'use strict';
 
-    /** @module esri map */
+    /** @module esri map 
+    * @description 
+     A collection of directives to help you use Esri maps and services in your Angular applications.
+		These directives can be used as-is if your mapping needs are simple, or as reference examples of the patterns that you can use to write your own directives that use the ArcGIS API for JavaScript
+    */
     angular.module('esri.map', []);
 
 })(angular);
