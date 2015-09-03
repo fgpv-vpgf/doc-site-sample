6b58a978d1ed6eee2e19f202c41f13ee201c5fbc
JackW <jack.wen@ec.gc.ca>
2015-09-03 16:42:11 -0400

    ref(processor): update output path and path for gcMethods
    use ngDocs to associate memberof from each gcMethods, update path and outputPath
    for each method

--- config/mergeJsToNg.js
+++ config/mergeJsToNg.js
@@ -26,23 +26,16 @@ module.exports = function jacksMergeProcessor(log) {
 			// find the memberof tag
 			// check type from doc.codeNode.type: e.g. FunctionDeclaration
 			
-			ngServiceDocs = _.filter(docs, { docType: 'ngService'});
+			// ngServiceDocs = _.filter(docs, { docType: 'ngService'});
 
-			ngDirectiveDocs = _.filter(docs, {docType: 'ngDirective'});
+			// ngDirectiveDocs = _.filter(docs, {docType: 'ngDirective'});
 
-			ngControllerDocs = _.filter(docs, {docType: 'ngController'});
+			// ngControllerDocs = _.filter(docs, {docType: 'ngController'});
 
-			jsDocs = _.filter(docs, {docType: 'js'});
+			ngDocs = _(docs).reject({docType: 'ngModule'})
+							.reject({docTYpe: 'componentGroup'})
+							.value();
 
-			jsDocs = _.filter(docs, function(doc) {
-				var tags = doc.tags.tags;
-				return _.find(tags, function(tag) {
-					if(tag.tagName === 'memberof') {
-						return true;
-					}
-					return false;
-				});
-			});
 
 
 			jsDocsWithMemberOf = _(docs).filter({docType: 'js'})
@@ -57,52 +50,49 @@ module.exports = function jacksMergeProcessor(log) {
 										})
 										.value();
 
-			docs.forEach( function(doc, idx, docs) {
-				if (doc.docType === 'js') {
-					// todo:
-					// 1. 	a. update path 
-					// 		b. create a new doctype for method
-					// 		c. ignore the doc, add directly to the angular stuff as dependencies
-					// 2. remove the element if it is added to a parent
+			jsDocsWithMemberOf.forEach( function(doc, idx) {
+				var tags = doc.tags.tags;
+				var memberofTag = _.find(tags, function(tag) {
+					if(tag.tagName === 'memberof'){
+						return true;
+					}
+				});
 
-					var tags = doc.tags.tags;
-					var memberofTag = _.find(tags, function(tag) {
-						if(tag.tagName === 'memberof'){
-							return true;
-						}
+				if (typeof memberofTag !== 'undefined'){
+					var parentName = memberofTag.description;
+
+					log.debug("memberof: " + parentName);
+					// find the associated doc e.g. service or directive
+					var parentIdx = _.findIndex(ngDocs, function(a) {
+						return a.name === parentName;
 					});
 
-					if (typeof memberofTag !== 'undefined'){
-						var parentName = memberofTag.description;
+					log.debug("doc id:"+ doc.id + ", parentIdx:" + parentIdx + ", memberof:" + parentName);
 
-						log.debug("memberof: " + parentName);
-						// find the associated doc e.g. service or directive
-						var parentIdx = _.findIndex(docs, function(a) {
-							return a.name === parentName;
+					if(parentIdx > -1){
+
+						var parentDoc = ngDocs[parentIdx];
+
+						if (! parentDoc.hasOwnProperty('methods')) {
+							// add methods
+							parentDoc.methods = [];
+						}
+
+						parentDoc.methods.push({
+							"name": doc.id,
+							"description": doc.description
 						});
 
-						log.debug("doc id:"+ doc.id + ", parentIdx:" + parentIdx + ", memberof:" + parentName);
-
-						if(parentIdx > -1){
-
-							var parentDoc = docs[parentIdx];
-
-							if (! parentDoc.hasOwnProperty('methods')) {
-								// add methods
-								parentDoc.methods = [];
-							}
-
-							parentDoc.methods.push({
-								"name": doc.id,
-								"description": doc.description
-							});
-						}
+						// if(_.isString(parentDoc.path)){
+							doc.path = parentDoc.path;
+							doc.outputPath = doc.path + doc.outputPath;
+						// }
 					}
-
-					// update doc type
-					doc.docType = "gcMethod";
-
 				}
+
+				// update doc type
+				doc.docType = "gcMethod";
+
 			});
 
 			return docs;
