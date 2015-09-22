643c88a8e37f5b9547d419ea1c60273ddb213513
Jack <Jack@>
2015-09-22 15:48:43 -0400

    feat(navigation): generate menu data for controller
    create menu, controller ctrlMain, filters related to menu generation.
    note: source from angular material design, modified and adapt to this example.
    filter are directly copy, will remove unnecessary items.

--- myDgeni/docs/app/js/app.js
+++ myDgeni/docs/app/js/app.js
@@ -40,5 +40,59 @@
                     // , controller: 'SomeController'
                 });
             });
+        })
+
+        .factory('menu', ['PAGES','API', '$location', '$rootScope', '$http', '$window', function(pages, api, $location, $rootScope, $http, $window) {
+            
+            // can be add in manually
+            var sections = [{
+                name: 'Getting Started',
+                url: '/getting-started',
+                type: 'heading'
+            }];
+
+            // generated from *-data.js
+            var apiDocs = [];
+            api.forEach(function(item) {
+                apiDocs.push(item);
+            });
+
+            sections.push({
+                name: 'API Reference',
+                type: 'heading',
+                children: [
+                {
+                    name: 'esri.maps',
+                    pages: apiDocs,
+                    type: 'toggle'
+                }]
+            });
+
+
+            return self = {
+                sections: sections
+            };
+        }])
+        .controller('ctrlMain', ['$scope', 'menu', function($scope, menu){
+            var self = this;
+            $scope.menu = menu;
+
+        }])
+        .filter('nospace', function () {
+          return function (value) {
+            return (!value) ? '' : value.replace(/ /g, '');
+          };
+        })
+        .filter('humanizeDoc', function() {
+          return function(doc) {
+            if (!doc) return;
+            if (doc.type === 'directive') {
+              return doc.name.replace(/([A-Z])/g, function($1) {
+                return '-'+$1.toLowerCase();
+              });
+            }
+            return doc.label || doc.name;
+          };
         });
+
 })(angular);
